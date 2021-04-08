5b5e9b2cd9a22d3dbd9ea67bf92066f31b0c28fb
Tekla Vass <Tekla.Vass@cloudflight.io>
2021-04-08 14:31:19 +0300

    added dao, dto

--- controllers/users/users_controller.go
+++ controllers/users/users_controller.go
@@ -2,15 +2,42 @@ package users
 
 import (
 	"github.com/gin-gonic/gin"
+	"github.com/vasstekla/bookstore_users-api/domain/users"
+	"github.com/vasstekla/bookstore_users-api/services"
+	"github.com/vasstekla/bookstore_users-api/utils/errors"
 	"net/http"
+	"strconv"
 )
 
 func CreateUser(c *gin.Context) {
-	c.String(http.StatusNotImplemented, "implement me!")
+	var user users.User
+	if err := c.ShouldBindJSON(&user); err != nil {
+		restErr := errors.NewBadRequestError("invalid json body")
+		c.JSON(restErr.Status, restErr)
+		return
+	}
+
+	result, saveErr := services.CreateUser(user)
+	if saveErr != nil {
+		c.JSON(saveErr.Status, saveErr)
+		return
+	}
+	c.JSON(http.StatusCreated, result)
 }
 
 func GetUser(c *gin.Context) {
-	c.String(http.StatusNotImplemented, "implement me!")
+	userId, userErr := strconv.ParseInt(c.Param("user_id"), 10, 64)
+	if userErr != nil {
+		err := errors.NewBadRequestError("userId should be a number")
+		c.JSON(err.Status, err)
+		return
+	}
+	user, getErr := services.GetUser(userId)
+	if getErr != nil {
+		c.JSON(getErr.Status, getErr)
+		return
+	}
+	c.JSON(http.StatusOK, user)
 }
 
 func SearchUser(c *gin.Context) {
